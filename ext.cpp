2a9e4310d85537d5c46503b8d4de5ed778629712
booqo <zoushaof@163.com>
2024-11-12 02:36:31 +0800

    更改函数名称，后缀增加underwater

--- ext.cpp
+++ ext.cpp
@@ -12,8 +12,15 @@
 #include <torch/extension.h>
 #include "rasterize_points.h"
 
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("rasterize_gaussians", &RasterizeGaussiansCUDA);
-  m.def("rasterize_gaussians_backward", &RasterizeGaussiansBackwardCUDA);
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {// 定义一个 Python 模块并注册 C++ 函数
+  m.def("rasterize_gaussians_underwater"  // Python 中的函数名
+  , &RasterizeGaussiansCUDA_underwater,              // 对应的 C++ 函数实现
+   "Render underwater Gaussian distributions");         //
+
+  m.def("rasterize_gaussians_backward_underwater",
+   &RasterizeGaussiansBackwardCUDA_underwater,
+   "Compute gradients for Gaussian rasterization (backward pass)");
   m.def("mark_visible", &markVisible);
 }
+
+//实现了三个函数，光栅化，梯度反向传播的光栅化，标记可视化
