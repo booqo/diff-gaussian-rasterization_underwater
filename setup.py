c3e916392e082dc1896294ad2351783931b35746
booqo <zoushaof@163.com>
2024-11-12 02:39:59 +0800

    更改包名称为diff_gaussian_rasterization_underwater

--- setup.py
+++ setup.py
@@ -12,23 +12,29 @@
 from setuptools import setup
 from torch.utils.cpp_extension import CUDAExtension, BuildExtension
 import os
-os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    name="diff_gaussian_rasterization",
-    packages=['diff_gaussian_rasterization'],
+    name="diff_gaussian_rasterization_underwater",  # 包名称
+    packages=['diff_gaussian_rasterization_underwater'],  # 包含的模块
     ext_modules=[
         CUDAExtension(
-            name="diff_gaussian_rasterization._C",
-            sources=[
-            "cuda_rasterizer/rasterizer_impl.cu",
-            "cuda_rasterizer/forward.cu",
-            "cuda_rasterizer/backward.cu",
-            "rasterize_points.cu",
-            "ext.cpp"],
-            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]})
-        ],
+            name="diff_gaussian_rasterization_underwater._C",  # 扩展模块名称
+            sources=[  # 源文件列表
+                "cuda_rasterizer/rasterizer_impl.cu",  # 核心 CUDA 实现
+                "cuda_rasterizer/forward.cu",         # 前向传播 CUDA 实现
+                "cuda_rasterizer/backward.cu",        # 反向传播 CUDA 实现
+                "rasterize_points.cu",                # 点渲染 CUDA 文件
+                "ext.cpp"                             # C++ 与 Python 的绑定实现
+            ],
+            extra_compile_args={
+                "nvcc": [
+                    "-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")
+                ]  # 额外的编译参数，包含第三方库 glm
+            }
+        )
+    ],
     cmdclass={
-        'build_ext': BuildExtension
+        'build_ext': BuildExtension  # 指定扩展构建方式
     }
 )
+
